899df60823e1e9eaa5d35c8c00218d42854bbdb3
comye1 <comye1@seoultech.ac.kr>
2022-01-21 17:43:51 +0900

    Starter Flutter app 2부 - interactivity, navigation, theme

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -16,6 +16,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
       home: RandomWords(),
     );
   }
@@ -34,14 +40,55 @@ class _RandomWordsState extends State<RandomWords> {
   // _로 시작하면 private
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = <WordPair>[];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            onPressed: _pushSaved,
+            icon: const Icon(Icons.list),
+            tooltip: 'Saved Suggestions',
+          )
+        ],
       ),
       body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      /*
+      Construct a MaterialPageRoute whose contents are defined by builder.
+      The values of builder, maintainState, and PageRoute.fullscreenDialog
+      must not be null.
+       */
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(tiles: tiles, context: context).toList()
+              : <Widget>[];
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
     );
   }
 
@@ -58,7 +105,7 @@ class _RandomWordsState extends State<RandomWords> {
         itemBuilder: (context, i) {
           // Add a one-pixel-high divider widget before each row
           // in the ListView.
-          if (i.isOdd) return Divider(); // 홀수 행 -> Divider 추가
+          if (i.isOdd) return const Divider(); // 홀수 행 -> Divider 추가
 
           // The syntax "i ~/ 2" divides i by 2 and returns an
           // integer result.
@@ -81,11 +128,32 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {
+        setState(() {
+          /*
+          Notify the framework that the internal state of this object has changed.
+          Whenever you change the internal state of a State object,
+          make the change in a function that you pass to setState:
+          setState(() { _myState = newValue; });
+           */
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
